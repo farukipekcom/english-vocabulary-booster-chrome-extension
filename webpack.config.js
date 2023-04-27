7807664c138bb28e788060a49c74f46b797058dc
Faruk İpek <farukipekweb@gmail.com>
2023-04-27 15:23:12 -0400

    add content script

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   entry: {
     popup: path.resolve("./src/popup/popup.tsx"),
     options: path.resolve("./src/options/options.tsx"),
+    contentScript: path.resolve("./src/contentScript/contentScript.tsx"),
   },
   module: {
     rules: [
