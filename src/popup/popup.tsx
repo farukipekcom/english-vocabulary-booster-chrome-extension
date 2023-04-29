90328b088be3aa1f5fbb42e8b7785c23da147bb9
Faruk İpek <farukipekweb@gmail.com>
2023-04-29 14:55:36 -0400

    added chrome storage permission and example

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./popup.css";
 
 const Popup = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    chrome.storage.sync.set({ name: name }, () => {
+      console.log("Name is set to: ", name);
+    });
+  };
+  useEffect(() => {
+    chrome.storage.sync.get(["name"], (res) => {
+      console.log(res.name);
+    });
+  }, []);
   return (
     <div>
       <h1>Hello World</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Enter a name" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
