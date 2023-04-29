584a77d36ba37c8fb91ce0ae6f293dbdfd5fa24f
Faruk İpek <farukipekweb@gmail.com>
2023-04-28 22:41:36 -0400

    added menu to options page

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Options from "./options";
+import { HashRouter as Router } from "react-router-dom";
 
 function init() {
   const appContainer = document.createElement("div");
@@ -10,7 +11,11 @@ function init() {
   }
   const root = createRoot(appContainer);
   console.log(appContainer);
-  root.render(<Options />);
+  root.render(
+    <Router>
+      <Options />
+    </Router>
+  );
 }
 
 init();
--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import "./options.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Settings from "./pages/settings";
 const Options = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <ul>
+        <li>
+          <a href="#/">Home</a>
+        </li>
+        <li>
+          <a href="#/settings">Settings</a>
+        </li>
+      </ul>
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/settings" element={<Settings />}></Route>
+      </Routes>
     </div>
   );
 };
