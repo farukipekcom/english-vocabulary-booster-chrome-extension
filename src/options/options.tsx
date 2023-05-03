08d9a0399ac9d65c82d28c5c04112f4e13d42339
Faruk İpek <farukipekweb@gmail.com>
2023-05-02 22:03:55 -0400

    added mywords page

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -3,10 +3,10 @@ import "../styles/main.scss";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Settings from "./pages/settings";
+import Mywords from "./pages/mywords";
 const Options = () => {
   return (
     <div>
-      <h1>Hello World</h1>
       <ul>
         <li>
           <a href="#/">Home</a>
@@ -14,11 +14,15 @@ const Options = () => {
         <li>
           <a href="#/settings">Settings</a>
         </li>
+        <li>
+          <a href="#/mywords">My Words</a>
+        </li>
       </ul>
       <Routes>
         <Route index element={<Home />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/settings" element={<Settings />}></Route>
+        <Route path="/mywords" element={<Mywords />}></Route>
       </Routes>
     </div>
   );
---
+++ src/options/pages/mywords.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Mywords() {
+  return <div>My Words</div>;
+}
+
+export default Mywords;
