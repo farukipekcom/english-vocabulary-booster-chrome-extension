395d0871a966ee6c804c85aec0f8fd426df1f95b
Faruk İpek <farukipekweb@gmail.com>
2023-05-26 22:30:52 -0400

    feat: add settings menu active class

--- src/options/components/SettingsMenu/SettingsMenu.tsx
+++ src/options/components/SettingsMenu/SettingsMenu.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "./SettingsMenu.module.scss";
+import {useLocation} from "react-router-dom";
 export default function SettingsMenu() {
+  let location = useLocation();
+  const menu = ["Profile", "List"];
+  const path = location.pathname.split("/")[2];
   return (
     <>
       <div className={styles.menu}>
-        <a href="#/settings/profile" className={`${styles.menuItem} ${styles.active}`}>
-          Profile
-        </a>
-        <a href="#/settings/list" className={styles.menuItem}>
-          List
-        </a>
+        {menu.map((item) => (
+          <a
+            key={item}
+            href={`#/settings/${item.replace(" ", "-").toLocaleLowerCase()}`}
+            className={`${styles.menuItem} ${item.replace(" ", "-").toLocaleLowerCase() === path && styles.active}`}>
+            {item}
+          </a>
+        ))}
       </div>
     </>
   );
